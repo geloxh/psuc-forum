7e15a3f31c63280f49a020081323940393c4aea4
Angelo <ace.blazado7@gmail.com>
2025-10-03 11:08:46 +0800

    organize css and commit partial about page

---
+++ about.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'includes/auth.php';
+    require_once 'includes/forum.php';
+
+    $auth = new Auth();
+    $forum = new Forum();
+    $user = $auth -> getCurrentUser();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About Page - PSUC Forum</title>
+        <!-- ===== CSS ===== -->
+        <link rel="stylesheet" href="assets/stylesheets/main.css">
+        <link rel="stylesheet" href="assets/stylesheets/dark-theme.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
